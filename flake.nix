7959c8b5b206e448c8b3583f55e8f6d5c724727c
Aleksander Heintz <alxandr@alxandr.me>
2026-06-13 14:12:48 +0200

    update workflows

--- flake.nix
+++ flake.nix
@@ -1,15 +1,24 @@
 {
-  description = "My personal NUR repository";
+  description = "Alxandr NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
+
+  outputs =
+    { self, nixpkgs }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in
     {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+      legacyPackages = forAllSystems (
+        system:
+        import ./default.nix {
+          pkgs = import nixpkgs { inherit system; };
+        }
+      );
+
+      packages = forAllSystems (
+        system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system}
+      );
+
       nixosModules = import ./nixos-modules;
       # homeModules = import ./home-modules;
       # darwinModules = import ./darwin-modules;
